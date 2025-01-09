0c15c3e01ab0b6609dde4298d282e9fc0c97ccfd
Alex <alexanderthestudent@yandex.ru>
2025-01-09 23:00:07 +0300

    feat(wall): coordinate correction
    Merged from branch `wall`, pull request #2

--- static/scripts/wall.js
+++ static/scripts/wall.js
@@ -1,61 +1,52 @@
-const videoContainer = document.getElementById("video-container");
+const previewsSection = document.getElementById("video-container");
 const calibrationButtons = document.getElementById("calibration").elements;
-const inputs = [];
+const statusBadge = document.getElementById("status");
+const gridAElement = document.getElementById("grid-a");
+const gridA = gridAElement.getContext("2d");
+const gridBElement = document.getElementById("grid-b");
+const gridB = gridBElement.getContext("2d");
+let camA;
+let camB;
+let touchDist = 0;
+let releaseDist = 0;
+let lastDist = Infinity;
 
-setupCalibration();
-// navigator.mediaDevices.getUserMedia({video: true, audio: false})
-setupCameras();
-track();
+init().catch(notifyErr)
 
-function setupCalibration() {
-  calibrationButtons.tl.addEventListener("click", async () =>
-    inputs.forEach(async (input) => {
-      const [person] = await trackWrists(input);
-      const [wrist, _] = person;
-      input.calibration.topLeft = [wrist.x, wrist.y];
+async function init() {
+  notify("initializing the UI")
+  setupCalibrationButtons();
 
-      input.figure.querySelector(".top-left")?.remove();
-      const corner = document.createElement("div");
-      corner.classList = ["corner top-left"];
-      corner.style.left = wrist.x * 100 + "%";
-      corner.style.top = wrist.y * 100 + "%";
-      input.figure.appendChild(corner);
-    }),
-  );
+  notify("asking for permissions")
+  await navigator.mediaDevices.getUserMedia({video: true, audio: false}).catch(() => {throw new Error("cameras missing")})
 
-  calibrationButtons.br.addEventListener("click", async () =>
-    inputs.forEach(async (input) => {
-      const [person] = await trackWrists(input);
-      const [_, wrist] = person;
-      input.calibration.bottomRight = [wrist.x, wrist.y];
+  notify("loading camera feeds")
+  await setupCameras();
 
-      input.figure.querySelector(".bottom-right")?.remove();
-      const corner = document.createElement("div");
-      corner.classList = ["corner bottom-right"];
-      corner.style.left = wrist.x * 100 + "%";
-      corner.style.top = wrist.y * 100 + "%";
-      input.figure.appendChild(corner);
-    }),
-  );
+  notifyOk("tracking")
+
+  while (true) {
+    await trackActions();
+    await new Promise(r => setTimeout(r, 50))
+  }
 }
 
 async function setupCameras() {
   const devices = await navigator.mediaDevices.enumerateDevices();
-  const cameras = devices.filter((device) => device.kind === "videoinput");
+  const videoInputs = devices.filter((device) => device.kind === "videoinput");
 
-  for (const camera of cameras) {
-    console.log('Found a camera')
-    const figure = document.createElement("figure");
-    figure.style.position = "relative";
+  const cameras = await Promise.all(videoInputs.map(async cam => {
+    const preview = document.createElement("figure");
+    preview.style.position = "relative";
 
-    const video = document.createElement("video");
+    const feed = document.createElement("video");
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: { deviceId: { exact: camera.deviceId } },
+      video: { deviceId: { exact: cam.deviceId } },
       audio: false,
     });
-    video.srcObject = stream;
-    video.play();
-    figure.appendChild(video);
+    feed.srcObject = stream;
+    feed.play();
+    preview.appendChild(feed);
 
     const detector = await poseDetection.createDetector(
       poseDetection.SupportedModels.MoveNet,
@@ -69,95 +60,327 @@ async function setupCameras() {
 
     const calibration = {
       topLeft: [0, 0],
+      topRight: [1, 0],
       bottomRight: [1, 1],
+      bottomLeft: [0, 1],
     };
 
-    videoContainer.appendChild(figure);
+    previewsSection.appendChild(preview);
 
-    inputs.push({ detector, video, figure, calibration, detections: [] });
+    return { detector, feed, preview, calibration };
+  }))
+
+  if (cameras.length === 0) {
+    throw new Error("both cameras missing")
+  }
+
+  if (cameras.length === 1) {
+    throw new Error("second camera missing")
+  }
+
+  if (cameras.length > 2) {
+    throw new Error("too many cameras")
+  }
+
+  [camA, camB] = cameras;
+
+  resizeGrid(gridAElement, gridA, camA.feed)
+  resizeGrid(gridBElement, gridB, camB.feed)
+
+  function resizeGrid(element, ctx, feed) {
+    element.style.width = "100%";
+    element.style.aspectRatio = `${feed.clientWidth} / ${feed.clientHeight}`;
+    setTimeout(() => {
+      ctx.canvas.width = element.clientWidth;
+      ctx.canvas.height = element.clientHeight;
+      ctx.lineWidth = 4;
+      gridClear(ctx);
+    }, 10)
   }
 }
 
-async function trackWrists(input) {
-  const poses = await input.detector.estimatePoses(input.video);
-  const width = input.video.videoWidth;
-  const height = input.video.videoHeight;
+async function trackWrists(cam) {
+  const poses = await cam.detector.estimatePoses(cam.feed);
+  const width = cam.feed.videoWidth;
+  const height = cam.feed.videoHeight;
 
-  const people = poses
+  return poses
     .map((person) => person.keypoints)
     .map((kp) => [
       kp.find((point) => point.name === "left_wrist"),
       kp.find((point) => point.name === "right_wrist"),
     ])
-    .map((inPx) => {
-      const inPercent = inPx.map((point) => ({
+    .map((inPx) => 
+      inPx.map((point) => ({
         ...point,
         x: point.x / width,
         y: point.y / height,
-      }));
-
-      return inPercent;
-    });
-
-  Array.from(input.figure.children)
-    .filter((child) => Array.from(child.classList).includes("marker"))
-    .forEach((marker) => marker.remove());
-
-  for (const person of people) {
-    const [leftWrist, rightWrist] = person;
-
-    const markerL = document.createElement("div");
-    markerL.classList = ["marker left"];
-    markerL.style.left = leftWrist.x * 100 + "%";
-    markerL.style.top = leftWrist.y * 100 + "%";
-
-    const markerR = document.createElement("div");
-    markerR.classList = ["marker right"];
-    markerR.style.left = rightWrist.x * 100 + "%";
-    markerR.style.top = rightWrist.y * 100 + "%";
-    input.figure.appendChild(markerL);
-    input.figure.appendChild(markerR);
-  }
-
-  input.detections = people.map(([leftWrist, rightWrist]) => {
-    const width = (input.calibration.bottomRight[0] - input.calibration.topLeft[0])
-    const height = (input.calibration.bottomRight[1] - input.calibration.topLeft[1])
-    const lx = (leftWrist.x - input.calibration.topLeft[0]) / width;
-    const ly = (leftWrist.y - input.calibration.topLeft[1]) / height;
-    const rx = (leftWrist.x - input.calibration.topLeft[0]) / width;
-    const ry = (leftWrist.y - input.calibration.topLeft[1]) / height;
-
-    return { leftWrist: {x: lx, y: ly}, rightWrist: {x: rx, y: ry}}
-  });
-
-  return people;
+      }))
+    );
 }
 
-let lastDistLeftWrist = 0;
-let threshold = 0.04;
+async function trackActions() {
+  const promiseA = findPoints(camA, gridA);
+  const promiseB = findPoints(camB, gridB);
+  const [pointsA, pointsB] = await Promise.all([promiseA, promiseB]);
 
-async function track() {
-  inputs.map(trackWrists);
+  const minDist = findMinDist(pointsA, pointsB);
 
-  setTimeout(track, 10);
-
-  if (inputs.length != 2) {
-    return
+  if (minDist !== 0 && minDist < touchDist && lastDist < touchDist) {
+    notifyOk("click registered")
   }
 
-  const [camA, camB] = inputs;
- 
-  if (camA.detections[0]?.leftWrist && camB.detections[0]?.leftWrist) {
-    const distLeftWrist = Math.sqrt(
-      (camA.detections[0].leftWrist.x - camB.detections[0].leftWrist.x) ** 2 +
-      (camA.detections[0].leftWrist.y - camB.detections[0].leftWrist.y) ** 2
-    );
+  if ((minDist === 0 && lastDist === 0) || (minDist > releaseDist && lastDist > releaseDist)) {
+    notifyOk("tracking")
+  }
 
-    if (distLeftWrist < threshold && lastDistLeftWrist < threshold) {
-      console.log('held')
-    }
-    if (distLeftWrist < threshold) {
-      lastDistLeftWrist = distLeftWrist;
+  lastDist = minDist;
+}
+
+function findMinDist(pointsA, pointsB) {
+  let minDist = 0;
+
+  const distances = pointsA.forEach(a => (pointsB.forEach(b => dist(a, b))))
+
+  function dist(a, b) {
+    const dx = a.x - b.x
+    const dy = a.y - b.y
+    const dist = dx**2 + dy**2 // I know it's not the right function, but I need to amplify the results
+
+    if (!minDist || dist < minDist) {
+      minDist = dist;
+      touch = [a.x, a.y];
     }
   }
+
+  return minDist;
+}
+
+function setupCalibrationButtons() {
+  calibrationButtons.tl.addEventListener("click", async () => {
+    let [[leftA, rightA]] = await trackWrists(camA)
+    let [[leftB, rightB]] = await trackWrists(camB)
+    camA.calibration.topLeft = [leftA.x, leftA.y];
+    camB.calibration.topLeft = [leftB.x, leftB.y];
+    gridDrawScreen(camA.calibration, gridA);
+    gridDrawScreen(camB.calibration, gridB);
+  });
+
+  calibrationButtons.tr.addEventListener("click", async () => {
+    let [[leftA, rightA]] = await trackWrists(camA)
+    let [[leftB, rightB]] = await trackWrists(camB)
+    camA.calibration.topRight = [rightA.x, rightA.y];
+    camB.calibration.topRight = [rightB.x, rightB.y];
+    gridDrawScreen(camA.calibration, gridA);
+    gridDrawScreen(camB.calibration, gridB);
+  });
+
+  calibrationButtons.bl.addEventListener("click", async () => {
+    let [[leftA, rightA]] = await trackWrists(camA)
+    let [[leftB, rightB]] = await trackWrists(camB)
+    camA.calibration.bottomLeft = [leftA.x, leftA.y];
+    camB.calibration.bottomLeft = [leftB.x, leftB.y];
+    gridDrawScreen(camA.calibration, gridA);
+    gridDrawScreen(camB.calibration, gridB);
+  });
+
+  calibrationButtons.br.addEventListener("click", async () => {
+    let [[leftA, rightA]] = await trackWrists(camA)
+    let [[leftB, rightB]] = await trackWrists(camB)
+    camA.calibration.bottomRight = [rightA.x, rightA.y];
+    camB.calibration.bottomRight = [rightB.x, rightB.y];
+    gridDrawScreen(camA.calibration, gridA);
+    gridDrawScreen(camB.calibration, gridB);
+  });
+
+  calibrationButtons.in.addEventListener("click", () => {
+    touchDist = lastDist;
+  })
+
+  calibrationButtons.out.addEventListener("click", () => {
+    releaseDist = lastDist;
+  })
+}
+
+function notifyOk(message) {
+  statusBadge.textContent = message
+  statusBadge.style.background = "var(--pico-primary)";
+  console.log(message)
+}
+
+function notify(message) {
+  statusBadge.textContent = message
+  statusBadge.style.background = null;
+  console.log(message)
+}
+
+function notifyErr(message) {
+  statusBadge.textContent = message
+  statusBadge.style.background = "light-dark(#d20f39, #f38ba8)";
+  console.error(message)
+}
+
+function gridClear(ctx) {
+  ctx.fillStyle = "#000";
+  ctx.strokeStyle = "transparent";
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  gridColorDefault(ctx);
+}
+function gridColorDefault(ctx) {
+  ctx.strokeStyle = "#aaa";
+}
+function gridColorAccent(ctx) {
+  ctx.strokeStyle = "#0f0";
+}
+
+function gridMoveTo(x, y, ctx) {
+  ctx.moveTo(x * ctx.canvas.width, y * ctx.canvas.height);
+}
+
+function gridLineTo(x, y, ctx) {
+  ctx.lineTo(x * ctx.canvas.width, y * ctx.canvas.height);
+}
+
+function gridDrawScreen(screen, ctx) {
+  gridClear(ctx);
+  drawRect(screen, ctx);
+}
+
+function drawRect(rect, ctx) {
+  gridColorDefault(ctx);
+  if (rect.active === true) {
+    gridColorAccent(ctx);
+  }
+
+  ctx.beginPath()
+  gridMoveTo(...rect.topLeft, ctx)
+  gridLineTo(...rect.topRight, ctx)
+  gridLineTo(...rect.bottomRight, ctx)
+  gridLineTo(...rect.bottomLeft, ctx)
+  gridLineTo(...rect.topLeft, ctx)
+  ctx.stroke()
+}
+
+async function findPoints(cam, ctx) {
+  gridClear(ctx);
+
+  const hands = await trackWrists(cam); // these are grouped by 2 - by people
+  const quadrantLocation = [
+    undefined,
+    {x: 1, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+  ]
+
+  const depth = 10;
+  const max = Math.pow(2, depth);
+
+  return recurse(cam.calibration, hands.flat(), ctx, depth - 1)
+    .map(point => ({x: point.x / max, y: point.y / max}))
+
+  function recurse(screen, hands, ctx, iterationsLeft) {
+    if (iterationsLeft <= 0) {
+      return [quadrantLocation[2]]
+    }
+
+    return cutIntoQuadrants(screen).map((quad, idx) => {
+      const point = hands.find(hand => pointInRect([hand.x, hand.y], quad));
+
+      quad.active = Boolean(point)
+      drawRect(quad, ctx);
+
+      if (!quad.active) {
+        return []
+      }
+
+      const outer = quadrantLocation[idx + 1]
+
+      return recurse(quad, hands, ctx, iterationsLeft-1).flatMap(inner => ({
+        x: outer.x * Math.pow(2, iterationsLeft) + inner.x,
+        y: outer.y * Math.pow(2, iterationsLeft) + inner.y,
+      }))
+    }).flat()
+  }
+}
+
+// implemented https://stackoverflow.com/questions/530396/how-to-draw-a-perspective-correct-grid-in-2d
+function cutIntoQuadrants(screen) {
+  const left = [screen.topLeft, screen.bottomLeft]
+  const bottom = [screen.bottomLeft, screen.bottomRight]
+  const top = [screen.topLeft, screen.topRight]
+  const right = [screen.topRight, screen.bottomRight]
+  const diagonal1 = [screen.topLeft, screen.bottomRight]
+  const diagonal2 = [screen.topRight, screen.bottomLeft]
+
+  const center = intersect(diagonal1, diagonal2)
+  const vanishingPoint1 = intersect(top, bottom)
+  const vanishingPoint2 = intersect(left, right)
+
+  if (vanishingPoint1 === null || vanishingPoint2 === null) {
+    console.debug('vanishing points are at infinity, meaning that either the world is perfect or the system is not calibrated')
+    return []
+  }
+
+  const primaryHorizontal = [center, vanishingPoint1]
+  const primaryVertical = [center, vanishingPoint2]
+
+  const leftCenter = intersect(left, primaryHorizontal)
+  const bottomCenter = intersect(bottom, primaryVertical)
+  const topCenter = intersect(top, primaryVertical)
+  const rightCenter = intersect(right, primaryHorizontal)
+
+  const quadrant1 = {
+    topLeft: topCenter,
+    topRight: screen.topRight,
+    bottomRight: rightCenter,
+    bottomLeft: center,
+  }
+  const quadrant2 = {
+    topLeft: screen.topLeft,
+    topRight: topCenter,
+    bottomRight: center,
+    bottomLeft: leftCenter,
+  }
+  const quadrant3 = {
+    topLeft: leftCenter,
+    topRight: center,
+    bottomRight: bottomCenter,
+    bottomLeft: screen.bottomLeft,
+  }
+  const quadrant4 = {
+    topLeft: center,
+    topRight: rightCenter,
+    bottomRight: screen.bottomRight,
+    bottomLeft: bottomCenter,
+  }
+
+  return [quadrant1, quadrant2, quadrant3, quadrant4]
+}
+
+function intersect(line1, line2) {
+  return math.intersect(...line1, ...line2)
+}
+
+// adapted from https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+function pointInRect(point, rect) {
+  function sign(p1, p2, p3) {
+    return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+  }
+
+  const left = sign(point, rect.bottomLeft, rect.topLeft);
+  const top = sign(point, rect.topLeft, rect.topRight);
+  const diagonal = sign(point, rect.topRight, rect.bottomLeft);
+  const right = sign(point, rect.topRight, rect.bottomRight);
+  const bottom = sign(point, rect.bottomRight, rect.bottomLeft);
+
+  const tri1HasNegative = (left < 0) || (top < 0) || (diagonal < 0);
+  const tri1HasPositive = (left > 0) || (top > 0) || (diagonal > 0);
+  const pointInTri1 = !(tri1HasNegative && tri1HasPositive)
+
+  const tri2HasNegative = (right < 0) || (bottom < 0) || (diagonal > 0);
+  const tri2HasPositive = (right > 0) || (bottom > 0) || (diagonal < 0);
+  const pointInTri2 = !(tri2HasNegative && tri2HasPositive)
+
+  return pointInTri1 || pointInTri2
 }
